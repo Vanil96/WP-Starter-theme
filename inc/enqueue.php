a01dc93da13ec51d16d14d1f12610739203b0112
“DM <d.motyka@testin.pl>
2022-06-22 15:05:32 +0200

    add scss structure

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -3,9 +3,6 @@
 defined('ABSPATH') || exit;
 
 
-
-
-
 function wpdocs_theme_name_scripts() {
     //scripts:   (if last parameter === true => load in footer)       
     wp_enqueue_script( 'theme-script', get_template_directory_uri() . '/assets/js/theme.js', array( 'jquery' ), '1.0.0', true );  //jquery depends
@@ -17,10 +14,8 @@ function wpdocs_theme_name_scripts() {
     wp_enqueue_style( 'theme-style', get_template_directory_uri() . '/assets/style/css/theme.css' );
     wp_enqueue_style( 'main-style', get_template_directory_uri() . '/assets/style/css/main.css' );
     wp_enqueue_style( 'custom-style', get_template_directory_uri() . '/style.css' );
-
-
-    
-
- 
 }
+
+
+
 add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
