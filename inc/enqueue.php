a6b21291f5077965ea2fe7b2529fbc60c5731cbc
dm <vanildm@gmail.com>
2025-02-09 21:07:13 +0100

    ref + fonts

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -7,8 +7,8 @@ function wpdocs_theme_name_scripts() {
     //scripts:   (if last parameter === true => load in footer)     
 
     //fonts
-    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Roboto:ital,wdth@1,87.5&display=swap', array(), null);
-    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&display=swap', array(), null);
+    wp_enqueue_style('google-font_primary', 'https://fonts.googleapis.com/css2?family=Roboto:ital,wdth@1,87.5&display=swap', array(), null);
+    wp_enqueue_style('google-font_secondary', 'https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&display=swap', array(), null);
 
 
     //js
